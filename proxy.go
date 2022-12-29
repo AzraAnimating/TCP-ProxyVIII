26ee5d6669f2a14403a5e34d94acd327af5debb1
Tobias Rempe <tobiasrempe@zyonicsoftware.com>
2022-12-30 00:34:47 +0100

    More Threading

--- proxy.go
+++ proxy.go
@@ -6,7 +6,7 @@ import (
 
 func openProxy() {
 
-	tcpAddress, addressError := net.ResolveTCPAddr("tcp", "0.0.0.0:12345")
+	tcpAddress, addressError := net.ResolveTCPAddr("tcp", "0.0.0.0:25565")
 	if addressError != nil {
 		return
 	}
@@ -22,82 +22,92 @@ func openProxy() {
 			continue
 		}
 
-		backendConnection, isConnected := openTCPConnection("mc.fuxelbau.de:25565")
-
-		if !isConnected {
-			continue
-		}
-
-		var buffer = make([]byte, 2048)
-		var serverBuffer = make([]byte, 2048)
-
-		var readFromClient = func() {
-			for {
-				readBytes, clientConnectionReadError := clientConnection.Read(buffer)
-				if clientConnectionReadError != nil {
-					clientConnection.Close()
-					backendConnection.Close()
-					//print("Read From Client Error ")
-					//println(clientConnectionReadError.Error())
-					return
-				}
-
-				if readBytes > 0 {
-					_, backendConnectionWriteError := backendConnection.Write(format(buffer, readBytes))
-					buffer = make([]byte, 2048)
-					if backendConnectionWriteError != nil {
-						clientConnection.Close()
-						backendConnection.Close()
-						//print("Write to Server Error ")
-						//println(backendConnectionWriteError.Error())
-						return
-					}
-				} else {
-					clientConnection.Close()
-					backendConnection.Close()
-				}
-			}
-		}
-
-		var readFromServer = func() {
-			for {
-				readBytes, backendConnectionReadError := backendConnection.Read(serverBuffer)
-				if err != backendConnectionReadError {
-					clientConnection.Close()
-					backendConnection.Close()
-					//print("Read From Server Error ")
-					//println(backendConnectionReadError.Error())
-					return
-				}
-
-				if readBytes > 0 {
-					_, clientWriteConnectionError := clientConnection.Write(format(serverBuffer, readBytes))
-					serverBuffer = make([]byte, 2048)
-					if clientWriteConnectionError != nil {
-						clientConnection.Close()
-						backendConnection.Close()
-						//print("Write to Client Error ")
-						//println(clientWriteConnectionError.Error())
-						return
-					}
-				} else {
-					clientConnection.Close()
-					backendConnection.Close()
-				}
-			}
-		}
-
-		go readFromServer()
-		go readFromClient()
-
+		openBackend(clientConnection)
 	}
 
 }
 
-func format(rawBuffer []byte, readBytes int) []byte {
-	var sendPacket []byte
-	for i := 0; i < readBytes; i++ {
-		sendPacket = append(sendPacket, rawBuffer[i])
+func openBackend(clientConnection net.Conn) {
+	backendConnection, isConnected := openTCPConnection("45.137.68.201:25561")
+
+	if !isConnected {
+		return
 	}
-	return sendPacket
+
+	go initProxy(clientConnection, backendConnection)
+}
+
+func initProxy(clientConnection net.Conn, backendConnection net.Conn) {
+	var buffer = make([]byte, 128)
+	var serverBuffer = make([]byte, 128)
+
+	var readFromClient = func() {
+		defer clientConnection.Close()
+		defer backendConnection.Close()
+		for {
+			readBytes, clientConnectionReadError := clientConnection.Read(buffer)
+			if clientConnectionReadError != nil {
+				clientConnection.Close()
+				backendConnection.Close()
+				//print("Read From Client Error ")
+				//println(clientConnectionReadError.Error())
+				return
+			}
+
+			if readBytes > 0 {
+				_, backendConnectionWriteError := backendConnection.Write(format(buffer, readBytes))
+				buffer = make([]byte, 128)
+				if backendConnectionWriteError != nil {
+					clientConnection.Close()
+					backendConnection.Close()
+					//print("Write to Server Error ")
+					//println(backendConnectionWriteError.Error())
+					return
+				}
+			} else {
+				clientConnection.Close()
+				backendConnection.Close()
+				return
+			}
+		}
+	}
+
+	var readFromServer = func() {
+		defer backendConnection.Close()
+		defer clientConnection.Close()
+		for {
+
+			readBytes, backendConnectionReadError := backendConnection.Read(serverBuffer)
+			if backendConnectionReadError != nil {
+				clientConnection.Close()
+				backendConnection.Close()
+				//print("Read From Server Error ")
+				//println(backendConnectionReadError.Error())
+				return
+			}
+
+			if readBytes > 0 {
+				_, clientWriteConnectionError := clientConnection.Write(format(serverBuffer, readBytes))
+				serverBuffer = make([]byte, 128)
+				if clientWriteConnectionError != nil {
+					clientConnection.Close()
+					backendConnection.Close()
+					//print("Write to Client Error ")
+					//println(clientWriteConnectionError.Error())
+					return
+				}
+			} else {
+				clientConnection.Close()
+				backendConnection.Close()
+				return
+			}
+		}
+	}
+
+	go readFromServer()
+	go readFromClient()
+}
+
+func format(rawBuffer []byte, readBytes int) []byte {
+	return rawBuffer[:readBytes]
 }
